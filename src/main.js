1b8bb725469ca825bb4b4703aaac6a91f1fed058
Alex Goodwin <hagoodwi@gmail.com>
2020-09-04 13:44:06 -0700

    add a form and button

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import SolarYears from './../src/js/solarYears.js';
+import SolarYears from './../src/js/solarYears.js';
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const ageInput = $("input#age").val();
+    let solar = new SolarYears(ageInput);
+    $("#output").text(solar.planet(solar.mercuryVal));
 
 
-
+  });
+});
